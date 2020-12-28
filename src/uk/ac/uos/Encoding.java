b0b8373ffc13ceea2b0bc987ccda5ca15cf7e99f
Bot-Toast <s203594@uos.ac.uk>
2020-12-28 21:26:39 +0000

    Added encoding and decoding functions

---
+++ src/uk/ac/uos/Encoding.java
@@ -0,0 +1,36 @@
+package uk.ac.uos;
+
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Encoding {
+
+
+    public static String b64Encoder(BigInteger toEncode) {
+        Base64.Encoder encoder = Base64.getEncoder();
+
+        String thing = toEncode.toString();
+
+        byte[] encodeBigIntBytes = thing.getBytes(StandardCharsets.UTF_8);
+
+        String encodedBytes = encoder.encodeToString(encodeBigIntBytes);
+
+        return encodedBytes;
+
+    }
+
+    public static BigInteger b64Decoder(String toDecode) {
+
+        byte[] bytese = Base64.getDecoder().decode(toDecode);
+
+        String Xeee = new String(bytese);
+
+        BigInteger bigInteger = new BigInteger(Xeee);
+
+        return bigInteger;
+
+    }
+
+}
