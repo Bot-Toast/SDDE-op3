9f425dd5619ccba00c72f780198a4c640154adfe
Bot-Toast <s203594@uos.ac.uk>
2020-12-31 20:31:26 +0000

    cleaning up code, adding last utility functions, refactoring some functions. GUI next.

---
+++ src/uk/ac/uos/BagOfHolding.java
@@ -0,0 +1,66 @@
+package uk.ac.uos;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BagOfHolding {
+
+
+    public static byte[] holdThings = new byte[128]; {
+
+    }
+
+
+    public static  List<String> pubKeyLoad = new ArrayList<String>();{
+
+
+
+    }
+
+    public static  BigInteger[] pubKeyBigInt = new BigInteger[2];{
+
+
+
+    }
+
+    public static List<String> prvKeyLoad = new ArrayList<String>(); {
+
+
+
+
+}
+
+    public static BigInteger[] prvKeyBigInt = new BigInteger[2] ;{
+
+
+    }
+
+    public static List<String> stringWriteList = new ArrayList<String>(); {
+
+
+
+
+    }
+
+    public static List<String> stringReadList = new ArrayList<String>(); {
+
+
+
+
+    }
+
+    public static List<String> tempArrs1 = new ArrayList<String>(); {
+
+
+
+
+    }
+
+    public static List<String> tempArrs2 = new ArrayList<String>(); {
+
+
+
+
+    }
+}
